25c1221d831adfb77a5a88b05519c93c6584a518
Gabriel Adrián Salcedo Varela <51538713+AdrianSalcedo@users.noreply.github.com>
2020-07-31 10:10:07 -0700

    Maple files
    Here we have the maple files for some conditions

--- R_file/Data_Plot.py
+++ R_file/Data_Plot.py
@@ -5,9 +5,8 @@ import pandas as pd
 
 with open('Tomato_data_det.csv', newline='') as File:
     reader = csv.reader(File)
-    #for row in reader:
-    #   print(row)
+    for row in reader:
+       print(row)
 print(reader)
 
 df=pd.read_csv('Tomato_data_det.csv', sep=',',header=None)
-
